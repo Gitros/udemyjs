62c0ca51acb0324452ecebecdb4a8a42f6eb0b2f
Gitros <31899790+Gitros@users.noreply.github.com>
2023-09-26 10:38:00 +0200

    clear

--- script.js
+++
@@ -1,9 +0,0 @@
-//var let const
-
-const color = 'niebieski';
-const name = 'jakub'
-let age = 24;
-let jedzenie = 'pizza';
-
-
-console.log(`Cześćm mam na imię ${name} lubie jeść ${jedzenie} i mam ${age} lata.`);
