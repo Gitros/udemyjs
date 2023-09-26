5efd54c23adb6ef60900823fc5e772d4fc1f1f55
Gitros <31899790+Gitros@users.noreply.github.com>
2023-09-26 10:46:24 +0200

    clear

--- script.js
+++
@@ -1,8 +0,0 @@
-const favColor = 'red';
-const meal = 'schabowy';
-const favDrink = 'pepsi';
-const function1 = 123;
-const name = 'Jakub';
-const age = '20';
-
-console.log(`Cześć jestem ${name} i mam ${age} lat.`);
