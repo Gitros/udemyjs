174da1f4110cda8e259702657b58d8d76b256064
Gitros <31899790+Gitros@users.noreply.github.com>
2023-09-27 11:08:38 +0200

    function

---
+++ script.js
@@ -0,0 +1,35 @@
+function test() {
+    console.log('moja funkcja');
+}
+
+test()
+
+function addNumbers(x, y) {
+    console.log(x + y);
+}
+
+addNumbers(5, 5)
+
+function showName(name) {
+    console.log(`podane imie to: ${name}`);
+}
+
+showName('Klaudia');
+
+function decreaseNumbers(x, y) {
+    console.log(x - y);
+}
+
+decreaseNumbers(10, 5);
+
+function multiplyNumbers(x, y) {
+    console.log(x * y);
+}
+
+multiplyNumbers(10, 5);
+
+function divisionNumbers(x, y) {
+    console.log(x / y);
+}
+
+divisionNumbers(10, 2)
