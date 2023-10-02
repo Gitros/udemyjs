d1711faba4feb4563aa762b896eb2c724dce915f
Gitros <31899790+Gitros@users.noreply.github.com>
2023-10-02 15:52:45 +0200

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+const p = document.querySelector('p');
+const btn = document.querySelector('#btn');
+
+console.log(p);
+console.log(btn);
+
+const liItems = document.querySelectorAll('li');
+
+console.log(liItems);
+
+const allP = document.querySelectorAll('p');
+
+console.log(allP);
