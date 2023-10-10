e2c1c92249a291e6d98e32ce26ff270e455e801f
Gitros <31899790+Gitros@users.noreply.github.com>
2023-10-10 18:02:08 +0200

    clear

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-const arrow = document.querySelector('fas fa-arrow-down');
-const button = document.querySelector('.arrow');
-const firstImage = document.querySelector('.item1')
-
-function add() {
-    firstImage.classList.toggle('show');
-
-    if (firstImage.classList.contains('show')) {
-        console.log("ok");
-    }
-    else {
-        console.log("nie ok");
-    }
-};
-
-button.addEventListener('click', add)
-
+const sizeUp = document.querySelector('.size-up');
+const sizeDown = document.querySelector('.size-down');
+const color = document.querySelector('.color');
+const p = document.querySelector('p');
